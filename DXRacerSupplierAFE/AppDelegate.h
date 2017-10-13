7e77fd6ae5d95f536b859c5268b81ed23ff667ef
ilovedxracer <ilovedxracer@ilovedxracerdeMac-mini.local>
2017-10-13 14:30:57 +0800

    Initial Commit

---
+++ DXRacerSupplierAFE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DXRacerSupplierAFE
+//
+//  Created by ilovedxracer on 2017/10/13.
+//  Copyright © 2017年 ilovedxracer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DXRacerSupplierAFE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DXRacerSupplierAFE
+//
+//  Created by ilovedxracer on 2017/10/13.
+//  Copyright © 2017年 ilovedxracer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
